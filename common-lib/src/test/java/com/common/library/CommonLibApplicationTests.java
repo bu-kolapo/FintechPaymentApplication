a408c24b76f3f297a95078dda217eb286cd70dac
Bukikol <kolapobukola2019@gmail.com>
2025-10-10 20:10:06 +0100

    microservices created

---
+++ common-lib/src/test/java/com/common/library/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
